c1bef3b718d13d2995c1dcb97aa1111c3614298c
Steven Martin <smartin132@gmail.com>
2015-11-11 19:56:44 -0500

    Moved bindings around. Much more comfortable backspace and esc key. Added macro for hash rocket and made colon easier to type

--- keyboard/ergodox/keymap_slowbad.h
+++ keyboard/ergodox/keymap_slowbad.h
@@ -2,51 +2,51 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KEYMAP(  // layer 0 : default
         // left hand
         EQL, 1,   2,   3,   4,   5,   FN22,
-        TAB, Q,   W,   F,   P,   G,   ESC,
-        LCTL,A,   R,   S,   T,   D,
-        LSFT,Z,   X,   C,   V,   B,   NO,
-        LGUI,GRV, BSLS,LALT,FN27,
-                                      LCTL,LALT,
-                                           HOME,
-                                 SPC, ENT, END,
+        TAB, Q,   W,   F,   P,   B,   ESC,
+        FN28,A,   R,   S,   T,   G,
+        LSFT,Z,   X,   C,   D,   V,   LBRC,
+        LGUI,LEFT,RGHT,LALT,FN27,
+                                      LCTL,HOME,
+                                           END,
+                                 SPC, FN28,LGUI,
         // right hand
-             FN24,6,   7,   8,   9,   0,   MINS,
+             FN26,6,   7,   8,   9,   0,   MINS,
              ESC, J,   L,   U,   Y,   SCLN,BSLS,
-                  H,   N,   E,   I,   O,   QUOT,
-             FN26,K,   M,   COMM,DOT, SLSH,RSFT,
-                       FN27,LEFT,DOWN,UP,  RGHT,
-        RALT,RCTL,
-        PGUP,
-        PGDN,DEL, BSPC
+                  M,   N,   E,   I,   O,   QUOT,
+             RBRC,K,   H,   COMM,DOT, SLSH,RSFT,
+                       FN27,RALT,DOWN,UP,  RGUI,
+        PGUP,RCTL,
+        PGDN,
+        DEL, ENT, BSPC
     ),
 
     KEYMAP(  // layer 1 : QWERTY
         // left hand
-        EQL, 1,   2,   3,   4,   5,   FN23,
+        EQL, 1,   2,   3,   4,   5,   FN24,
         TAB, Q,   W,   E,   R,   T,   ESC,
-        LCTL,A,   S,   D,   F,   G,
-        LSFT,Z,   X,   C,   V,   B,   NO,
+        FN28,A,   S,   D,   F,   G,
+        LSFT,Z,   X,   C,   V,   B,   LBRC,
         LGUI,GRV, BSLS,LALT,FN27,
                                       LCTL,LALT,
                                            HOME,
-                                 SPC, ENT, END,
+                                 SPC, FN28,END,
         // right hand
-             NO,  6,   7,   8,   9,   0,   MINS,
+             FN23,6,   7,   8,   9,   0,   MINS,
              ESC, Y,   U,   I,   O,   P,   BSLS,
                   H,   J,   K,   L,   SCLN,QUOT,
-             FN26,  N,   M,   COMM,DOT, SLSH,RSFT,
-                       FN27,LEFT,DOWN,UP,  RGHT,
-        RALT,RCTL,
-        PGUP,
-        PGDN,DEL, BSPC
+             RBRC,N,   M,   COMM,DOT, SLSH,RSFT,
+                       FN27,RALT,DOWN,UP,  RGUI,
+        PGUP,RCTL,
+        PGDN,
+        DEL, ENT, BSPC
     ),
 
     KEYMAP(  // layer 2 : Gaming
         // left hand
         F7,  1,   2,   3,   4,   5,   6,
-        TRNS,Q,   W,   E,   R,   T,   7,
+        TAB, Q,   W,   E,   R,   T,   7,
         LALT,A,   S,   D,   F,   G,
-        TRNS,Z,   X,   C,   V,   B,   ESC,
+        LSFT,Z,   X,   C,   V,   B,   ESC,
         F5,  F6,  0,   9,   8,
                                       F1,  F2,
                                            F3,
@@ -59,50 +59,50 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                        RGUI,TRNS,TRNS,TRNS,TRNS,
         HOME,END,
         PGUP,
-        PGDN,ENTER,BSPC
+        PGDN,ENT, BSPC
     ),
 
     KEYMAP(  // layer 3 : function and symbol keys
         // left hand
-        TRNS,F1,  F2,  F3,  F4,  F5,  TRNS,
-        TRNS,BSLS,SLSH,LBRC,RBRC,FN13,TRNS,
-        TRNS,FN11,FN12,FN9, FN10, GRV,
-        TRNS,FN1, FN2, FN3, FN4, FN5, TRNS,
-        TRNS,TRNS,TRNS,TRNS,TRNS,
-                                      TRNS,TRNS,
-                                           TRNS,
-                                 TRNS,TRNS,TRNS,
+        NO,  F1,  F2,  F3,  F4,  F5,  NO,  
+        NO,  BSLS,SLSH,LBRC,RBRC,FN13,NO,  
+        NO,  FN3, FN4, FN9, FN10,GRV,
+        NO,  FN1, FN2, FN11,FN12,FN5, NO,  
+        NO,  NO,  NO,  TRNS,TRNS,
+                                      NO,  NO,  
+                                           NO,  
+                                 NO,  TRNS,NO,  
         // right hand
-             F12, F6,  F7,  F8,  F9,  F10, TRNS,
-             TRNS,NO,  EQL, FN14,MINS,FN15,F11,
-                  NO,  NO,  NO,  NO,  NO,  F12,
-             TRNS,FN6, FN7, FN8, FN9, FN10,TRNS,
-                       TRNS,TRNS,TRNS,TRNS,TRNS,
-        TRNS,TRNS,
-        TRNS,
-        TRNS,TRNS,TRNS
+             F12, F6,  F7,  F8,  F9,  F10, NO,  
+             NO,  FN17,EQL, FN15,MINS,FN14,F11,
+                  LEFT,RGHT,DOWN,UP,  FN16,F12,
+             NO,  FN6, FN7, FN8, FN9, FN10,NO,  
+                       TRNS,MPRV,VOLD,VOLU,MNXT,  
+        NO,NO,
+        NO,  
+        NO,MUTE,MPLY
     ),
 
 
     KEYMAP(  // layer 4: Numpad
         // left hand
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,FN0,
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-        TRNS,TRNS,TRNS,TRNS,TRNS,
-                                      TRNS,TRNS,
-                                           TRNS,
-                                 TRNS,TRNS,TRNS,
+        FN0, NO,  NO,  NO,  NO,  NO,  NO,  
+        NO,  NO,  NO,  NO,  NO,  NO,  NO,  
+        NO,  NO,  NO,  NO,  NO,  NO,  
+        NO,  NO,  NO,  NO,  NO,  NO,  NO,  
+        NO,  NO,  NO,  NO,  NO,  
+                                      NO,  NO,  
+                                           TRNS,  
+                                 TRNS,NO,  TRNS,  
         // right hand
-             TRNS,NLCK,PSLS,PAST,PAST,PMNS,BSPC,
-             TRNS,NO,  P7,  P8,  P9,  PMNS,BSPC,
+             FN26,NLCK,PSLS,PAST,PAST,PMNS,BSPC,
+             NO,  NO,  P7,  P8,  P9,  PMNS,BSPC,
                   NO,  P4,  P5,  P6,  PPLS,PENT,
-             TRNS,NO,  P1,  P2,  P3,  PPLS,PENT,
+             NO,  NO,  P1,  P2,  P3,  PPLS,PENT,
                        P0,  PDOT,SLSH,PENT,PENT,
-        TRNS,TRNS,
-        TRNS,
-        TRNS,TRNS,TRNS
+        NO,  NO,  
+        TRNS,  
+        TRNS,TRNS,  TRNS
     ),
 
 };  
@@ -111,18 +111,23 @@ enum function_id {
     TEENSY_KEY,
 };
 
+enum macro_id {
+    HASH_ROCKET,
+}; 
+
 static const uint16_t PROGMEM fn_actions[] = {
     [0]  =   ACTION_FUNCTION(TEENSY_KEY),                 // FN0  = Teensy key
 
-    [20]  =   ACTION_LAYER_ON(0, ON_RELEASE),                        // FN2  = set Colemak
-    [21]  =   ACTION_LAYER_OFF(0, ON_RELEASE),                        // FN2  = set Colemak
-    [22]  =   ACTION_LAYER_ON(1, ON_RELEASE),                        // FN2  = set QWERTY layer
-    [23]  =   ACTION_LAYER_OFF(1, ON_RELEASE),                        // FN2  = set Colemak
-    [24]  =   ACTION_LAYER_ON(2, ON_RELEASE),                        // FN3  = set Gaming layer
-    [25]  =   ACTION_LAYER_OFF(2, ON_RELEASE),                        // FN2  = set Colemak
-    [26]  =   ACTION_LAYER_TOGGLE(4),                     // FN4  = toggle Numpad layer
+    [20]  =   ACTION_LAYER_ON(0, ON_RELEASE),             // FN20 = push Colemak
+    [21]  =   ACTION_LAYER_OFF(0, ON_RELEASE),            // FN21 = pop Colemak
+    [22]  =   ACTION_LAYER_ON(1, ON_RELEASE),             // FN22 = push QWERTY 
+    [23]  =   ACTION_LAYER_OFF(1, ON_RELEASE),            // FN23 = pop Colemak
+    [24]  =   ACTION_LAYER_ON(2, ON_RELEASE),             // FN24 = push Gaming
+    [25]  =   ACTION_LAYER_OFF(2, ON_RELEASE),            // FN25 = pop Gaming
+    [26]  =   ACTION_LAYER_TOGGLE(4),                     // FN26 = toggle Numpad layer
 
-    [27] =   ACTION_LAYER_MOMENTARY(3),                  // FN30  = Symbols layer
+    [27]  =   ACTION_LAYER_MOMENTARY(3),                  // FN27  = Symbols layer
+    [28]  =   ACTION_MODS_TAP_KEY(MOD_LCTL, KC_ESC),
 
 };
 
@@ -142,6 +147,8 @@ static const uint16_t PROGMEM fn_actions_3[] = {
     [13] =   ACTION_MODS_KEY(MOD_LSFT,   KC_GRAVE),
     [14] =   ACTION_MODS_KEY(MOD_LSFT,   KC_EQUAL),
     [15] =   ACTION_MODS_KEY(MOD_LSFT,   KC_MINUS),
+    [16] =   ACTION_MODS_KEY(MOD_LSFT,   KC_SCOLON),
+    [17] =   ACTION_MACRO(HASH_ROCKET),
     
 };
 
@@ -155,6 +162,15 @@ void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
         print("not supported.\n");
     }
 };
+
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
+    if (record->event.pressed) {
+        switch (id) {
+            case HASH_ROCKET: return MACRO( T(EQUAL), D(LSHIFT), T(DOT), U(LSHIFT), END );
+        }
+    }
+    return MACRO_NONE;
+}
 
 #define FN_ACTIONS_SIZE       (sizeof(fn_actions)   / sizeof(fn_actions[0]))
 #define FN_ACTIONS_3_SIZE     (sizeof(fn_actions_3) / sizeof(fn_actions_3[0]))
